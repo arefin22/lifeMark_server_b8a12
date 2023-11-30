d47bc2ed5e879d3d171493fe56afc65b7efe44d0
arefin22 <shamsularefin500@gmail.com>
2023-11-30 23:40:35 +0600

    Added Appointment

--- src/models/Banner.js
+++ src/models/Banner.js
@@ -4,32 +4,24 @@ const { Schema, default: mongoose } = require('mongoose')
 const BannerSchema = new Schema({
     bannerName: {
         type:String,
-        required :true
     },
     bannerImg : {
         type:String,
-        required:true
     },
     bannerTitle : {
         type:String,
-        required: true
     },
     couponCodeName:{
         type: String,
-        required:true
     },
     couponRate:{
         type:Number,
-        required:true
     },
     bannerDescription:{
         type:String,
-        required:true
     },
     isActive:{
         type:String,
-        enum: ["true", "false"],
-        required:true
     },
 })
 
